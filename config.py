d80a603c3dbbd31944020cfd027a1bcfcc237a7d
Liz2222 <adhiamboliz3@gmail.com>
2022-05-19 01:45:44 +0300

    updates

--- config.py
+++ config.py
@@ -8,20 +8,13 @@ class Config:
     General parent configuration parent class
     """
     SECRET_KEY ='123456789'
-    app.config['DEBUG'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    
+    SQLALCHEMY_DATABASE_URI = ''
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/pitches'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    # SIMPLEMDE_JS_IIFE = True
-    # SIMPLEMDE_USE_CDN = True
-    # @staticmethod
-    # def init_app(app):
-    #     pass
+   
 class ProdConfig(Config):
    
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1) 
+    
     DEBUG= True
 
 
