66307b62f2bbc3441e4cffe82c39bfa93a990016
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-18 15:42:22 +0300

    registration and login classes

---
+++ app/auth/form.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, DateTimeField, FileField
+from wtforms.validators import DataRequired, EqualTo, Length,Email
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=10)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    repeat_password = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Sign-Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+
+    submit = SubmitField('Login')    
