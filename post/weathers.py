e96ac6b292802ddc8a686efede95ad51692c00d2
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-05-19 01:46:41 +0300

    added table name

--- post/weathers.py
+++ post/weathers.py
@@ -4,7 +4,9 @@ from flask import Flask, render_template, request
 
 from app import db
 
+
 class City(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+     __tablename__ = 'city'
+id = db.Column(db.Integer, primary_key=True)
+name = db.Column(db.String(50), nullable=False)
 
