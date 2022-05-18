5db8024638f8aaff5b41ffcdb5c7c65cb1ef3fe6
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-18 16:03:49 +0300

    register/login files

--- app/auth/form.py
+++ app/auth/form.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextA
 from wtforms.validators import DataRequired, EqualTo, Length,Email
 
 
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=10)])
     email = StringField('Email', validators=[DataRequired(), Email()])
